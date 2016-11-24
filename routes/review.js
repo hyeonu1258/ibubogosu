3480e2b444a9a38de23eb691133c045d85fee5c9
Hyeonu Ryu <Hyeonu Ryu>
2016-11-24 16:39:26 +0900

    review detail api complete

--- routes/review.js
+++ routes/review.js
@@ -19,40 +19,70 @@ router.route('/:review_id/update')
 module.exports = router;
 
 function imageReviewList(req, res) {
-  var imageReviewQuery = 'select r.review_id, r.review_content, r.prod_rating, r.like_count, url.review_image_id, url.review_image_url, u.user_id, u.nickname, u.prof_image_url, u.type, u.age, u.height, u.weight, p.prod_id, p.prod_name, p.shopping_site_name from review r join product p on r.prod_id=p.prod_id and p.prod_id=(select prod_id from review where review_id=?) and r.image_exist_chk=1 join user u on r.user_id=u.user_id and u.type=? join review_image_url url on r.review_id=url.review_id';
+  var imageReviewQuery = 'select r.review_id, r.review_content, r.prod_rating, r.like_count, url.review_image_id, url.review_image_url, u.user_id, u.nickname, u.prof_image_url, u.type, u.age, u.height, u.weight, p.prod_id, p.prod_name, p.prod_purchase_site_url, p.shopping_site_name from review r join product p on r.prod_id=p.prod_id and p.prod_id=(select prod_id from review where review_id=?) and r.image_exist_chk=1 join user u on r.user_id=u.user_id and u.type=? join review_image_url url on r.review_id=url.review_id';
   var countQuery = 'select count(*) as cnt from review r join product p on r.prod_id=p.prod_id and p.prod_id=(select prod_id from review where review_id=?) and r.prod_rating=? join user u on r.user_id=u.user_id and u.type=?';
+  var comReviewQuery = 'select r.review_id, r.review_content, r.prod_rating, u.user_id, u.nickname, u.prof_image_url, u.type, u.age, u.height, u.weight from review r join product p on r.prod_id=p.prod_id and p.prod_id=(select prod_id from review where review_id=?) and r.image_exist_chk=0 join user u on r.user_id=u.user_id and u.type=?';
   var revList, response;
   var rating = new Array();
   var index = [0, 1, 2, 3, 4, 5];
 
   pool.getConnection(function(err, conn) {
-    if(err)
-      console.error('Error', err);
-    else {
-      conn.query(imageReviewQuery, [req.params.review_id, req.params.type],
-        function(err, rows) {
-          if(err)
-            console.log('query1 failed ', err);
-          else {
-            if(rows.length > 0) {
-              revList = rows;
-              var a = function(i) {
-                conn.query(countQuery, [req.params.review_id, i*20, req.params.type],
-                  function(err, rows) {
-                    if(err)
-                      console.log('query2 failed ', err);
-                    else
-                      if(rows.length > 0)
-                        rating[i] = rows[0].cnt;
-                    if(i == 5)
-                        res.send({revList : revList, rating : rating});
-                });
+    if(err) {
+      console.log('connection err', err);
+      res.send({err : {code : 1, msg : 'db connection err'}, data : {}});
+    } else {
+      async.series([
+        function(callback) {
+          conn.query(imageReviewQuery, [req.params.review_id, req.params.type],
+            function(err, rows) {
+              if(err) {
+                console.log("image reivew query err", err);
+                res.send({err : {code : 1, msg : 'image review query err'}, data : {}});
+              } else {
+                if(rows.length <= 0) {
+                  console.log('image review query result is 0', err);
+                  res.send({err : {code : 1, msg : 'image review query result is 0'}, data : {}});
+                } else
+                  callback(null, rows);
               }
-              for(i in index)
-                a(i);
-            }
+            });
+        },
+        function(callback) {
+          var a = function(i) {
+            conn.query(countQuery, [req.params.review_id, i, req.params.type],
+              function(err, rows) {
+                if(err) {
+                  console.log('rating query err', err);
+                  res.send({err : {code : 1, msg : 'rating query err'}, data : {}});
+                } else {
+                  rating[i] = rows[0].cnt;
+                  if(i == 5)
+                    callback(null, rating);
+                }
+            });
           }
-        });
+          for(i in index) a(i);
+        },
+        function(callback) {
+          conn.query(comReviewQuery, [req.params.review_id, req.params.type],
+            function(err, rows) {
+              if(err) {
+                console.log('common review query err', err);
+                res.send({err : {code : 1, msg : 'common review query err'}, data : {}});
+              } else {
+                if(rows.length <= 0) {
+                  console.log('common review query result is 0', err);
+                  res.send({err : {code : 1, msg : 'common review query result is 0'}, data : {}});
+                } else
+                  callback(null, rows);
+              }
+          });
+        }
+      ],
+      function(err, result) {
+        var comRevList;
+        res.send({err : {code : 0, msg : {}}, data : {revList : result[0], rating : result[1], comRevList : result[2]}});
+      });
     }
   });
 }
