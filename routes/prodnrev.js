46b5f6448462e94bc8f4a56c920798f0ad39bda9
Hyeonu Ryu <Hyeonu Ryu>
2016-12-02 18:21:56 +0900

    reviewFeed api complete

--- routes/prodnrev.js
+++ routes/prodnrev.js
@@ -6,64 +6,105 @@ var router = express.Router();
 
 /* GET home page. */
 router.route('/:type/:count')
-    .get(prodList)
-    .post(revList);
+    .get(prodList);
 
 function prodList(req, res) {
     prodQuery = 'select p.prod_id, p.prod_name, p.shopping_site_name, p.folder_count, count(review_id) as review_count from product p join review r on p.prod_id = r.prod_id and r.type = ? group by p.prod_id limit ?, ?';
+    revQuery = 'select url.review_id, url.review_image_id, url.review_image_url from review_image_url url join review r on r.review_id = url.review_id join product p on r.prod_id = p.prod_id where r.prod_id = ? group by r.review_id;';
+    productList = [];
+
     pool.getConnection(function(err, conn) {
-        if (err) {
-            console.log('db connection err', err);
-            res.send({
-                err: {
-                    code: 1,
-                    msg: 'db connection err'
-                },
-                data: {}
-            });
-        } else {
-            conn.query(prodQuery, [req.params.type, (req.params.count-1)*50, 50],
-                function(err, rows) {
-                    console.log(err, rows);
-                    if (err) {
-                        console.log('product query err ', err);
-                        res.send({
-                            err: {
-                                code: 1,
-                                msg: 'product query err'
-                            },
-                            data: {}
-                        });
-                        conn.release();
-                    } else {
-                        if (rows.length <= 0) {
-                            console.log('product query result is 0', rows);
-                            res.send({
-                                err: {
-                                    code: 1,
-                                    msg: 'product query result is 0'
+            if (err) {
+                console.log('db connection err', err);
+                res.send({
+                    err: {
+                        code: 1,
+                        msg: 'db connection err'
+                    },
+                    data: {}
+                });
+            } else {
+                async.waterfall([
+                        function(callback) {
+                            conn.query(prodQuery, [req.params.type, (req.params.count - 1) * 50, 50],
+                                function(err, rows) {
+                                    console.log(err, rows);
+                                    if (err) {
+                                        console.log('product query err ', err);
+                                        var err = {
+                                            err: {
+                                                code: 1,
+                                                msg: 'product query err'
+                                            },
+                                            data: {}
+                                        };
+                                        callback(err);
+                                    } else {
+                                        if (rows.length <= 0) {
+                                            console.log('product query result is 0', rows);
+                                            var err = {
+                                                err: {
+                                                    code: 1,
+                                                    msg: 'product query result is 0'
+                                                },
+                                                data: {}
+                                            };
+                                            callback('no product');
+                                        } else {
+                                            console.log('product query success ', rows);
+                                            callback(null, rows);
+                                        }
+                                    }
+                                });
+                        },
+                        function(arg, callback) {
+                            console.log(arg);
+                            async.every(arg, function(product, done) {
+                                        console.log(product);
+                                        console.log(product.prod_id);
+                                        conn.query(revQuery, product.prod_id, function(err, rows) {
+                                                if (err) {
+                                                    console.log('review query err', err);
+                                                    var err = {
+                                                        err: {
+                                                            code: 1,
+                                                            msg: 'review query err'
+                                                        },
+                                                        data: {}
+                                                    };
+                                                done(err);
+                                            } else {
+                                                console.log('review query success', rows);
+                                                product.review = rows;
+                                                productList.push(product);
+                                                done(null, !err);
+                                            }
+                                        });
                                 },
-                                data: {}
-                            });
+                                function(err, result) {
+                                    if (err) {
+                                        console.log('every prodnrev err', err);
+                                        callback(err);
+                                    } else {
+                                        console.log('every prodnver success', result);
+                                        callback(null, result);
+                                    }
+                                });
+                    }],
+                    function(err, result) {
+                        if (err) {
+                            console.log('waterfall prodnrev err ', err);
+                            res.send(err);
                             conn.release();
                         } else {
-                            console.log('product query success ', rows);
-                            res.send({
-                                err: {
-                                    code: 0,
-                                    msg: ''
-                                },
-                                data: rows
-                            });
+                            console.log('waterfall prodnrev success ');
+                            res.send(productList);
+                            conn.release();
                         }
-                    }
-                });
+
+                    });
         }
     });
-}
-
-function revList(req, res) {
-
 }
 
 module.exports = router;
