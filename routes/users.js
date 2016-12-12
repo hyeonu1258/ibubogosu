b6d57210d5c7e451b83060139608324f381917a8
Hyeonu Ryu <Hyeonu Ryu>
2016-12-12 15:13:00 +0900

    firebase id를 기본키로 설정, prodnrev에 사용자 타입과 일치하는 리뷰 피드백

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,8 @@ router.route('/update')
       .post(deleteUser)
 
 function registUser(req, res) {
+    registQuery = 'insert into user(user_id, sns_token, prof_image_url, type, height, weight) values(?,?,?,?,?,?)';
+    inserts = [req.body.user_id, req.body.sns_token, req.body.prof_image_url, req.body.type, req.body.height, req.body.weight];
     pool.getConnection(function(err, conn) {
         if (err) {
             console.log(err);
@@ -24,12 +26,14 @@ function registUser(req, res) {
                     conn.query('select user_id from user where user_id=?', [req.body.user_id], function(err, rows) {
                         if (err) callback(msg(1, 'query err: ' + err, []));
                         else
-                            if (rows.length < 1)  callback(null, 'success');
-                            else                  callback(msg(0, 'login success', []));
-                    })
+                            if(rows.length < 1) callback(null, 'success');
+                            else
+                              if(req.body.kakao_id) callback(null, 'kakao login usr_id exist');
+                              else                  callback(msg(0, 'login success', []));
+                    });
                 },
                 function(callback) {
-                    conn.query('insert into user(user_id, sns_token, prof_image_url, type, weight, height) values(?,?,?,?,?,?)', [req.body.user_id, req.body.sns_token, req.body.prof_image_url, req.body.type, req.body.height, req.body.weight], function(err, rows) {
+                    conn.query(registQuery, inserts, function(err, rows) {
                         if (err)  callback(msg(1, 'query err: ' + err, []));
                         else      callback(null, 'regist user success');
                     });
